1401c0f775a65e1cffd52059a97c19913e5bbfb1
rynecates38 <rynecates38@gmail.com>
2026-08-14 13:43:48 -0500

    v1.5.9 Release: Fix embedded desktop and taskbar RLM logo icon in PyInstaller spec

--- rlm_importer_ui.py
+++ rlm_importer_ui.py
@@ -33,7 +33,7 @@ try:
 except Exception:
     pass
 
-VERSION = "1.5.8"
+VERSION = "1.5.9"
 
 # 👑 Premium Gold & Obsidian Theme Design System Tokens
 BG_DARK = "#0c0a09"          # Warm obsidian charcoal
